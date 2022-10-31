d80f4a19df54489a12b591fdd6956e36e167bf13
claradenken <claradenken@gmail.com>
2022-10-31 14:39:27 +0100

    w3 d1 sql intro

---
+++ Week_3/SQL/SQL_Intro.sql
@@ -0,0 +1,6 @@
+use sakila;
+select description from sakila.film;
+select name as language from sakila.language;
+select store_id from sakila.store;		# The company has two stores.
+select * from sakila.staff;				# The company has two employees.
+select first_name from sakila.staff;
